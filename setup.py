b0065fffe68c3de4962c7f13522c6e905b343032
Florian Alu <alu@prolibre.com>
2020-09-08 15:59:31 +0200

    Add in manifest woff woff2 tff ico Add in requirements.txt and setup.py -> djangorestframework-datatables==0.5.2

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         "phonenumbers==8.12.9", "Pillow==7.2.0", "django-crispy-forms==1.9.2", "urllib3==1.25.10",
         "django-select2==7.4.2", "django-bootstrap-modal-forms==2.0.0", "django-widget-tweaks==1.4.8",
         "django-bootstrap-datepicker-plus==3.0.5", "django-simple-menu==1.2.1", "django-extensions==3.0.8",
-        "django_weasyprint==1.0.1", "nobinobi-staff==0.1.3.1", "nobinobi-core==0.1.0"
+        "djangorestframework-datatables==0.5.2", "django_weasyprint==1.0.1", "nobinobi-staff==0.1.3.1", "nobinobi-core==0.1.0"
     ],
     zip_safe=False,
     keywords='nobinobi-child',
