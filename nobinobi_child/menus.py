d9cb46175415f086da9a4ff74e012a4611679a71
Florian Alu <alu@prolibre.com>
2021-01-21 11:09:14 +0100

    add method to check if user has permission to view menu.

--- nobinobi_child/menus.py
+++ nobinobi_child/menus.py
@@ -16,43 +16,52 @@ from django.urls import reverse
 from django.utils.translation import gettext as _
 from menu import Menu, MenuItem
 
-Menu.add_item("main",
-              MenuItem(
-                  title=_("Children"),
-                  url="/child/",
-                  icon="fa fa-child",
-                  children=[
-                      MenuItem(
-                          title=_("Children list"),
-                          url=reverse("nobinobi_child:Child_list"),
-                          icon="fas fa-list"),
-                      MenuItem(
-                          title=_("Child picture"),
-                          url=reverse("nobinobi_child:child_picture_select"),
-                          icon="fas fa-camera"),
-                      MenuItem(
-                          title=_("Information of the day"),
-                          url=reverse("nobinobi_child:InformationOfTheDay_list"),
-                          icon="fas fa-list"),
-                      MenuItem(
-                          title=_("Absences"),
-                          url=reverse("nobinobi_child:Absence_list"),
-                          icon="fas fa-list"),
-                  ])
-              )
+from nobinobi_child.utils import has_view_child, has_view_staff
+
+Menu.add_item(
+    "main",
+    MenuItem(
+        title=_("Children"),
+        url="/child/",
+        icon="fa fa-child",
+        children=[
+            MenuItem(
+                title=_("Children list"),
+                url=reverse("nobinobi_child:Child_list"),
+                icon="fas fa-list"),
+            MenuItem(
+                title=_("Child picture"),
+                url=reverse("nobinobi_child:child_picture_select"),
+                icon="fas fa-camera"),
+            MenuItem(
+                title=_("Information of the day"),
+                url=reverse("nobinobi_child:InformationOfTheDay_list"),
+                icon="fas fa-list"),
+            MenuItem(
+                title=_("Absences"),
+                url=reverse("nobinobi_child:Absence_list"),
+                icon="fas fa-list"),
+        ],
+        check=lambda request: has_view_child(request)
+    )
+)
+
 # staff
-Menu.add_item("staff",
-              MenuItem(
-                  title=_("Staff"),
-                  url="/staff/",
-                  icon="fa fa-user",
-                  children=[
-                      MenuItem(
-                          title=_("Staffs list"),
-                          url=reverse("nobinobi_child:Staff_list"),
-                          icon="fas fa-list"),
-                  ])
-              )
+Menu.add_item(
+    "staff",
+    MenuItem(
+        title=_("Staff"),
+        url="/staff/",
+        icon="fa fa-user",
+        children=[
+            MenuItem(
+                title=_("Staffs list"),
+                url=reverse("nobinobi_child:Staff_list"),
+                icon="fas fa-list"),
+        ],
+        check=lambda request: has_view_staff(request)
+    )
+)
 # Menu.add_item("main", MenuItem("Staff Only",
 #                                reverse("reports.views.staff"),
 #                                check=lambda request: request.user.is_staff))
